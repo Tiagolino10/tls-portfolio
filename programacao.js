6e8762136ee3ef9f5133256bf0c18b4e90bc659a
Tiago Lino <tiagolino2014@gmail.com>
2025-06-29 16:15:22 -0300

    portfolio
     Este é meu portfolio e também trabalho da faculdade Uninter.

---
+++ programacao.js
@@ -0,0 +1,8 @@
+// Seleciona o botão pelo ID
+const botaoLink = document.getElementById("BotaoLink");
+
+// Adiciona um ouvinte de evento para o clique
+botaoLink.addEventListener("click", function () {
+  // Redireciona para a URL desejada
+  window.location.href = "index2.html";
+});
---
+++ programacao2.js
@@ -0,0 +1,9 @@
+// botão curriculo
+const CurriculoBotaoLink = document.getElementById("CurriculoBotaoLink");
+
+CurriculoBotaoLink.addEventListener("click", function () {
+  //  // Abre a URL em uma nova aba/janela
+  window.open("index3.html", "_blank");
+});
+
+// window.open("index2.html", "_blank"); "_blank" abre uma nova página.
